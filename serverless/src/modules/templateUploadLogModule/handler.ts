4332ab6cf9b5c1a7c64d4686efda11ecee1892a0
asubugade <asubugade@shipco.com>
2025-04-02 17:38:19 +0530

    add api/templateuploadlog/list api

--- serverless/src/modules/templateUploadLogModule/handler.ts
+++ serverless/src/modules/templateUploadLogModule/handler.ts
@@ -18,7 +18,30 @@ module.exports.listByTemplateTypeHandler = async (event, context, callback) => {
         .status(HttpStatusCodes.BAD_REQUEST)
         .json({ errors: errors.array() }))
     }
-    return await templateUploadLogService.templateUploadLogListing(event);
+    return await templateUploadLogService.templateUploadLogListByTemplateType(event);
+  } catch (error) {
+    response = {
+      statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+      body: JSON.stringify({ error: error.message }),
+    };
+    callback(null, response);
+  }
+};
+
+module.exports.templateUploadLogListHandler = async (event, context, callback) => {
+  let response;
+  try {
+    context.callbackWaitsForEmptyEventLoop = false;
+    await oConnectDB();
+    check("iOffset", "Offset is required").not().isEmpty();
+    check("iLimit", "Offset is required").not().isEmpty();
+    const errors = validationResult(event);
+    if (!errors.isEmpty()) {
+      callback(null, event
+        .status(HttpStatusCodes.BAD_REQUEST)
+        .json({ errors: errors.array() }))
+    }
+    return await templateUploadLogService.templateUploadLogList(event);
   } catch (error) {
     response = {
       statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
